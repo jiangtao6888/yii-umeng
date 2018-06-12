40f401dad9bbd1ee4e213414405d7c7425890d10
jiangtao <jiangtao6888@gmaill.com>
2018-06-12 16:58:05 +0800

    update

--- Umeng.php
+++ Umeng.php
@@ -77,6 +77,32 @@ class Umeng extends Component
 		}
 	}
 
+	function sendAndroidListcast ()
+	{
+		try {
+			$unicast = new AndroidListcast();
+			$unicast->setAppMasterSecret($this->appMasterSecret);
+			$unicast->setPredefinedKeyValue("appkey", $this->appkey);
+			$unicast->setPredefinedKeyValue("timestamp", $this->timestamp);
+			// Set your device tokens here
+			$unicast->setPredefinedKeyValue("device_tokens", '');
+			$unicast->setPredefinedKeyValue("ticker", "Android unicast ticker");
+			$unicast->setPredefinedKeyValue("title", "Android unicast title");
+			$unicast->setPredefinedKeyValue("text", "Android unicast text");
+			$unicast->setPredefinedKeyValue("after_open", "go_app");
+			// Set 'production_mode' to 'false' if it's a test device.
+			// For how to register a test device, please see the developer doc.
+			$unicast->setPredefinedKeyValue("production_mode", "true");
+			// Set extra fields
+			$unicast->setExtraField("test", "helloworld");
+			print("Sending unicast notification, please wait...\r\n");
+			$unicast->send();
+			print("Sent SUCCESS\r\n");
+		} catch (\Exception $e) {
+			print("Caught exception: " . $e->getMessage());
+		}
+	}
+
 	function sendAndroidFilecast ()
 	{
 		try {
@@ -223,6 +249,30 @@ class Umeng extends Component
 	{
 		try {
 			$unicast = new IOSUnicast();
+			$unicast->setAppMasterSecret($this->appMasterSecret);
+			$unicast->setPredefinedKeyValue("appkey", $this->appkey);
+			$unicast->setPredefinedKeyValue("timestamp", $this->timestamp);
+			// Set your device tokens here
+			$unicast->setPredefinedKeyValue("device_tokens", "xx");
+			$unicast->setPredefinedKeyValue("alert", "IOS 单播测试");
+			$unicast->setPredefinedKeyValue("badge", 0);
+			$unicast->setPredefinedKeyValue("sound", "chime");
+			// Set 'production_mode' to 'true' if your app is under production mode
+			$unicast->setPredefinedKeyValue("production_mode", "false");
+			// Set customized fields
+			$unicast->setCustomizedField("test", "helloworld");
+			print("Sending unicast notification, please wait...\r\n");
+			$unicast->send();
+			print("Sent SUCCESS\r\n");
+		} catch (\Exception $e) {
+			print("Caught exception: " . $e->getMessage());
+		}
+	}
+
+	function sendIOSListcast ()
+	{
+		try {
+			$unicast = new IOSListcast();
 			$unicast->setAppMasterSecret($this->appMasterSecret);
 			$unicast->setPredefinedKeyValue("appkey", $this->appkey);
 			$unicast->setPredefinedKeyValue("timestamp", $this->timestamp);
